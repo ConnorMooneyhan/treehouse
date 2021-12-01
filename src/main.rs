18b7af2882ee3682505217d37038665cd761eb3b
Connor Mooneyhan <connmoo@gmail.com>
2021-12-01 07:24:51 -0500

    Make visitor_list growable and loop input

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,56 @@
+#![warn(clippy::all, clippy::pedantic)]
 use std::io;
 
-fn main() {
-    println!("Hello, what's your name?");
-    let your_name = read_string();
-    let visitor_list = ["bert", "steve", "fred", "connor"];
-    if visitor_list.contains(&your_name.as_str()) {
-        println!("Welcome to the Treehouse, {}.", your_name);
-    } else {
-        println!("Sorry, you are not on the list.");
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
     }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
+fn main() {
+    let mut visitor_list = vec![
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+        Visitor::new(
+            "connor",
+            "Greetings to you, Sir Connor. Your mission awaits you.",
+        ),
+    ];
+
+    loop {
+        println!("Hello, what's your name?");
+        let name = read_string();
+
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, "New friend"));
+                }
+            }
+        }
+    }
+
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
 
 fn read_string() -> String {
